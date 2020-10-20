41d706b6a9488acfaa133809e53b7859f2416ce7
Monika Kowalska <m.kowalska@futureit.agency>
2020-10-21 00:47:08 +0200

    clean Vue template + add axios

---
+++ src/views/SearchApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <BooksSearchInput />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BooksSearchInput from "@/components/BooksSearchInput.vue";
+
+export default {
+  name: "SearchApp",
+  components: {
+    BooksSearchInput
+  }
+};
+</script>
